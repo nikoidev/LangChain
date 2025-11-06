e8dc9299417a3e8b16fd8b664de24b06b1bd7754
Nicolas A. Urbaez A. <aran.nick15@gmail.com>
2025-11-06 10:42:19 +0100

    Añadir formato y salida de ejemplo para la plantilla de eslogan creativo en marketing

--- Tema2/prompt_templates.py
+++ Tema2/prompt_templates.py
@@ -6,3 +6,6 @@ prompt = PromptTemplate(
     template= template,
     input_variables= ["producto"]
 )
+
+prompt_lleno = prompt.format(producto="zapatos deportivos")
+print(prompt_lleno)  # Salida: Eres un experto en marketing. Sugiere un eslogan creativo para un producto zapatos deportivos
